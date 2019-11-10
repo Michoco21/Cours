62fa94e0c0107b8b3106d629680c4c17b0c71e37
Michoco <michelleattlan@hotmail.com>
2019-11-10 19:59:40 +0100

    Modification du fichier des commandes

--- commandes_vi.php
+++ commandes_vi.php
@@ -1,9 +1,60 @@
-<!DOCTYPE html>
-<head>
-<title>Titre de ma page</title>
-</head>
-<body>
-Commandes VI
-</body>
-</html>
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <h1>Commandes VI</h1>
+ <div class="ligne1">
+   <h2>Déplacement dans un fichier</h2>
+    <h3>Déplacement à l'aide des flèches</h3>
+    <ul>
+      <li>La touche h pour vous déplacer vers la gauche.</li>
+      <li>La touche l pour vous déplacer vers la droite.</li>
+      <li>La touche j pour vous déplacer vers le bas.</li>
+      <li>La touche k pour vous déplacer vers le haut.</li>
+    </ul>
 
+    <h3>Déplacement mot par mot</h3>
+    <ul>
+      <li>Appuyez sur w ("word") pour déplacer le curseur mot par mot vers la droite.</li>
+      <li>Appuyez sur b ("back") pour déplacer le curseur mot par mot vers la gauche.</li>
+      <li>Appuyez sur W ou sur B pour déplacer le curseur du signe de ponctuation le plus proche au blanc suivant ou précédent.</li>
+      <li>Appuyez sur e ("end") pour positionner le curseur sur le dernier caractère du mot en cours.</li>
+    </ul>
+
+    <h3>Déplacement vers le début ou la fin d'une ligne</h3>
+    <ul>
+      <li>Appuyez sur ^ pour amener le curseur au début de la ligne en cours.</li>
+      <li>Appuyez sur $ pour amener le curseur à la fin de la ligne en cours.</li>
+    </ul>
+
+    <h3>Déplacement ligne par ligne</h3>
+    <ul>
+      <li>Appuyez sur la touche Return pour positionner le curseur au début de la ligne suivante.</li>
+    </ul>
+
+    <h3>Déplacement vers la gauche</h3>
+    <ul>
+      <li>Appuyez sur la barre d'espacement pour déplacer le curseur d'un caractère vers la droite.</li>
+    </ul>
+
+    <h3>Déplacement vers la droite</h3>
+    <ul>
+      <li>Appuyez sur la barre d'espacement pour déplacer le curseur d'un caractère vers la droite.</li>
+    </ul>
+
+    <h3>Déplacement vers le haut</h3>
+    <ul>
+      <li>Appuyez sur H ("high") pour amener le curseur en haut de l'écran.</li>
+    </ul>
+
+    <h3>Déplacement vers le centre</h3>
+    <ul>
+      <li>Appuyez sur M ("middle") pour amener le curseur au centre de l'écran.</li>
+    </ul>
+
+    <h3>Déplacement vers le bas</h3>
+    <ul>
+      <li>Appuyez sur L ("low") pour amener le curseur en bas de l'écran.</li>
+    </ul>
+  </div>
